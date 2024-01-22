d65908a595941b3c6030a76ae65b0bc2aa45376d
User <user@Users-MacBook-Air.local>
2024-01-22 07:49:30 -0500

    populated schema.sql file

---
+++ schema.sql
@@ -0,0 +1,30 @@
+
+
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+use employees_db;
+
+CREATE TABLE department(
+    id INT  PRIMARY KEY,
+    name VARCHAR(30) 
+);
+
+CREATE TABLE role(
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT ,
+    CONSTRAINT FK_departmentID FOREIGN KEY (department_id) REFERENCES department(id) 
+);
+
+
+CREATE TABLE employee(
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id  INT ,
+    CONSTRAINT FK_roleID FOREIGN KEY (role_id) REFERENCES role(id),
+    CONSTRAINT FK_managerID FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
